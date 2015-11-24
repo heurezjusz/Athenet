25e9032cfc2e8cdfd6588c8333132e7d955f4065
Marcin Papierzyński <marcin.papierzynski@student.uw.edu.pl>
2015-11-24 19:00:11 +0100

    Fixed file name in load.py

--- rat1/load.py
+++ rat1/load.py
@@ -5,5 +5,5 @@ Program for training and saving net to a file.
 import cPickle
 import LeNet.convolutional_mlp as conv
 
-weights, biases = cPickle.load(open('conv.pkl'))
+weights, biases = cPickle.load(open('net.pkl'))
 conv.test_lenet5(weights, biases)
--- rat1/train_and_save.py
+++ rat1/train_and_save.py
@@ -5,7 +5,7 @@ Program for training and saving net to a file.
 import cPickle
 import LeNet.convolutional_mlp as conv
 
-layers = conv.evaluate_lenet5(n_epochs=2)
+layers = conv.evaluate_lenet5(n_epochs=1)
 f = open('net.pkl', 'w')
 weights = [layers[i].W.get_value() for i in xrange(4)]
 biases = [layers[i].b.get_value() for i in xrange(4)]
