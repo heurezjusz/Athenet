ab46ef9dc92525240ff968341eb2f2a21a8d1d25
marta <marta.cieslak.1101@gmail.com>
2015-12-13 19:24:27 +0100

    rat1 and rat2

---
+++ src/sparsifying/rat.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def set_zeros_on_layer(layer, percentage, order):
+    W = layer.W
+    percentile = np.percentile([order(a) for a in W.flat], percentage)
+    W[order(W) < percentile] = 0
+    layer.W = W   
+    
+def set_zeros_on_network(network, percentage, order):
+    weights = np.concatenate([layer.W.flatten() for layer in network.weighted_layers])
+    percentile = np.percentile([order(a) for a in weights.flat], percentage)
+    for layer in network.weighted_layers:
+        W = layer.W
+        W[order(W) < percentile] = 0
+        layer.W = W 
+       
+
+def rat1_on_network(network, percentage):
+    set_zeros_on_network(network, percentage, abs)
+    
+def rat2_on_network(network, percentage):
+    weights = np.concatenate([layer.W.flatten() for layer in network.weighted_layers])
+    mean = np.mean(weights)
+    set_zeros_on_network(network, percentage, lambda x: abs(mean - x))
+    
+def rat1_on_layers(network, percentage):
+    for layer in network.weighted_layers:
+        set_zeros_on_layer(layer, percentage, abs)
+        
+def rat2_on_layers(network, percentage):
+    for layer in network.weighted_layers:
+        mean = np.mean(layer.W.flatten())
+        set_zeros_on_layer(layer, percentage, lambda x: abs(mean - x))
+        
+    
+        
