be3bedc9e4f026e85b5c3a45c35b606d3ce5545c
marta <marta.cieslak.1101@gmail.com>
2015-12-13 19:26:59 +0100

    draft of main

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.sparsifying.rat import rat1_on_network
+
+def sparsify(network, algorithm='rat1', percentage = 7, *args):
+    if algorithm == 'rat1':
+        rat1_on_network(network, percentage)
+    
+    return network
