d58ca896682e0a371abd9dc4c18e2d0b2b541bc7
Marcin Papierzyński <marcin.papierzynski@student.uw.edu.pl>
2016-01-01 20:14:01 +0100

    Added static fields and methods to MNIST data loader

--- athenet/utils/mnist_data_loader.py
+++ athenet/utils/mnist_data_loader.py
@@ -9,30 +9,29 @@ import theano.tensor as T
 from athenet.utils import DataLoader, load_data
 
 
-_MNIST_FILENAME = os.path.join(os.path.dirname(__file__),
-                               '../../bin/mnist.pkl.gz')
-_MNIST_ORIGIN = ('http://www.iro.umontreal.ca/~lisa/deep/data/mnist/'
-                 'mnist.pkl.gz')
-
-
-def _mnist_shared_dataset(data):
-    """Create shared variables from given data.
-
-    data: Data consisting of pairs (input, output).
-    return: Theano shared variables created from data.
-    """
-    data_in, data_out = data
-    data_in = np.resize(data_in, (data_in.shape[0], 1, 28, 28))
-    shared_in = theano.shared(
-        np.asarray(data_in, dtype=theano.config.floatX), borrow=True)
-    shared_out = theano.shared(
-        np.asarray(data_out, dtype=theano.config.floatX), borrow=True)
-    return shared_in, T.cast(shared_out, 'int32')
-
-
 class MNISTDataLoader(DataLoader):
     """MNIST data loader."""
-    def __init__(self, filename=_MNIST_FILENAME, url=_MNIST_ORIGIN):
+    MNIST_filename = os.path.join(os.path.dirname(__file__),
+                                  '../../bin/mnist.pkl.gz')
+    MNIST_origin = ('http://www.iro.umontreal.ca/~lisa/deep/data/mnist/'
+                    'mnist.pkl.gz')
+
+    @staticmethod
+    def _mnist_shared_dataset(data):
+        """Create shared variables from given data.
+
+        data: Data consisting of pairs (input, output).
+        return: Theano shared variables created from data.
+        """
+        data_in, data_out = data
+        data_in = np.resize(data_in, (data_in.shape[0], 1, 28, 28))
+        shared_in = theano.shared(
+            np.asarray(data_in, dtype=theano.config.floatX), borrow=True)
+        shared_out = theano.shared(
+            np.asarray(data_out, dtype=theano.config.floatX), borrow=True)
+        return shared_in, T.cast(shared_out, 'int32')
+
+    def __init__(self, filename=MNIST_filename, url=MNIST_origin):
         """Create MNIST data loader.
 
         filename: Name of a file with MNIST data.
@@ -41,11 +40,11 @@ class MNISTDataLoader(DataLoader):
         train_set, valid_set, test_set = load_data(filename, url)
 
         self.test_set_in, self.test_set_out =\
-            _mnist_shared_dataset(test_set)
+            self._mnist_shared_dataset(test_set)
         self.valid_set_in, self.valid_set_out =\
-            _mnist_shared_dataset(valid_set)
+            self._mnist_shared_dataset(valid_set)
         self.train_set_in, self.train_set_out =\
-            _mnist_shared_dataset(train_set)
+            self._mnist_shared_dataset(train_set)
 
         self.train_set_size =\
             self.train_set_in.get_value(borrow=True).shape[0]
