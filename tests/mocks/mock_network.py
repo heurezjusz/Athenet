db1319d33d81121afd2adedc9eaac15c8a8828f5
marta <marta.cieslak.1101@gmail.com>
2016-01-06 20:37:03 +0100

    basic mocks

---
+++ tests/mocks/mock_network.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class NetworkMock(object):
+    def __init__(self, weighted_layers=None):
+        
+        if weighted_layers is None:
+            weighted_layers = np.array()
+            
+        self.weighted_layers = weighted_layers
+        self.layers = weighted_layers
+    
+class LayerMock(object):
+    def __init__(self, weights=None, biases=None):
+        self.W = weights if weights is not None else np.array()
+        self.B = biases if biases is not None else np.array()
+        
+    
