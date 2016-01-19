d937efbdc5f64be1b71cef6668dfe1022f328482
marta <marta.cieslak.1101@gmail.com>
2016-01-19 12:20:09 +0100

    documentation

--- src/sparsifying/sparsify_smallest.py
+++ src/sparsifying/sparsify_smallest.py
@@ -1,6 +1,19 @@
 import numpy as np
 
 def set_zeros_on_layer(layer, percentage, order):
+    """
+    Change weights in layer to zeros
+    
+    This function, for given order of weights,
+    change the set percentage of the smallest to zeros or,
+    if that is not possible, takes the ceiling of such number
+    
+    :param WeightedLayer layer: layer for sparsifying
+    :param percentage: percentage of weights to be changes to zeros
+    :param order: order of weights
+    :type percentage: int, float
+    """
+    
     if percentage == 0:
         return
 
@@ -10,10 +23,24 @@ def set_zeros_on_layer(layer, percentage, order):
     layer.W = W   
     
 def set_zeros_on_network(network, percentage, order):
+    """
+    Change weights in network to zeros
+    
+    This function, for given order of weights, 
+    change the set percentage of the smallest to zeros or, 
+    if that is not possible, takes the ceiling of such number
+    
+    :param Network network: network for sparsifying
+    :param percentage: percentage of weights to be changes to zeros
+    :param order: order of weights
+    :type percentage: int, float
+    :type order: function
+    """
     if percentage == 0:
         return
 
-    weights = np.concatenate([layer.W.flatten() for layer in network.weighted_layers])
+    weights = np.concatenate(
+                [layer.W.flatten() for layer in network.weighted_layers])
     percentile = np.percentile([order(a) for a in weights.flat], percentage)
     for layer in network.weighted_layers:
         W = layer.W
@@ -21,18 +48,64 @@ def set_zeros_on_network(network, percentage, order):
         layer.W = W 
        
 def sparsify_smallest_on_network(network, percentage):
+    """
+    Change smallest weights in network to zeros
+    
+    This function changes weights in such a way,
+    that for the whole network the set percentage of the smallest of them are zeros,
+    or, if that is not possible, takes the ceiling of such a number
+    
+    :param Network network: network for sparsifying
+    :param percentage: percentage of weights to be changes to zeros
+    :type percentage: int, float
+    """
     set_zeros_on_network(network, percentage, abs)
     
 def sparsify_nearest_to_network_mean(network, percentage):
-    weights = np.concatenate([layer.W.flatten() for layer in network.weighted_layers])
+    """
+    Change weights close to mean in network to zeros
+    
+    This function changes weights in such a way,
+    that for the whole network the set percentage of the closest of them to mean are zeros,
+    or, if that is not possible, takes the ceiling of such a number
+    
+    :param Network network: network for sparsifying
+    :param percentage: percentage of weights to be changes to zeros
+    :type percentage: int, float
+    """
+    
+    weights = np.concatenate(
+                [layer.W.flatten() for layer in network.weighted_layers])
     mean = np.mean(weights)
     set_zeros_on_network(network, percentage, lambda x: abs(mean - x))
     
 def sparsify_smallest_on_layers(network, percentage):
+    """
+    Change smallest weights in each layer to zeros.
+    
+    This function for each layer changes weights in such a way, 
+    that at least the set percentage of the smallest of them are zeros.
+    
+    :param Network network: network for sparcifying
+    :param percentage: percentage of weights to be changed to zeros
+    :type percentage: int, float
+    """
+    
     for layer in network.weighted_layers:
         set_zeros_on_layer(layer, percentage, abs)
         
 def sparsify_nearest_to_layer_mean(network, percentage):
+    """
+    Change smallest close to mean in each layers to zeros
+    
+    This function for each layer changes weights in such a way,
+    that the set percentage of the closest to mean are zeros
+    or, if that is not possible, takes the ceiling of such a number
+    
+    :param Network network: network for sparcifying
+    :param percentage: percentage of weights to be changed to zeros
+    :type percentage: int, float
+    """
     for layer in network.weighted_layers:
         mean = np.mean(layer.W.flatten())
         set_zeros_on_layer(layer, percentage, lambda x: abs(mean - x))
