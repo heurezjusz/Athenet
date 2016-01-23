bdfcf3290df0d4eb74f3b4b9eda83bc089d10de8
Marcin Papierzyński <marcin.papierzynski@student.uw.edu.pl>
2016-01-23 18:22:30 +0100

    Simplify LeNet learning demo

--- demo/mnist_learning_lenet.py
+++ demo/mnist_learning_lenet.py
@@ -1,23 +1,11 @@
 """Training LeNet on MNIST data."""
 
-from athenet import Network
-from athenet.layers import ReLU, Softmax, MaxPool, FullyConnectedLayer, \
-    ConvolutionalLayer
+import athenet
 from athenet.utils import MNISTDataLoader
+from athenet.models import lenet
 
 
-network = Network([
-    ConvolutionalLayer(image_shape=(28, 28, 1), filter_shape=(5, 5, 20)),
-    ReLU(),
-    MaxPool(poolsize=(2, 2)),
-    ConvolutionalLayer(filter_shape=(5, 5, 50)),
-    ReLU(),
-    MaxPool(poolsize=(2, 2)),
-    FullyConnectedLayer(n_out=500),
-    ReLU(),
-    FullyConnectedLayer(n_out=10),
-    Softmax(),
-])
-
+network = athenet.models.lenet(trained=False)
 network.data_loader = MNISTDataLoader()
+
 network.train(learning_rate=0.1, n_epochs=10, batch_size=300)
