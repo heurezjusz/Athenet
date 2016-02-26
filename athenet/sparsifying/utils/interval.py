573af13dc22d9db3875fffec05ede4ca77e6c2ef
Wojciech Głogowski <wojciech.glogowski@students.mimuw.edu.pl>
2016-02-26 21:09:05 +0100

    -Interval class ready, tests in progress

--- athenet/sparsifying/utils/interval.py
+++ athenet/sparsifying/utils/interval.py
@@ -1,6 +1,7 @@
 """Intervals in Theano including special functions for sparsifying."""
 
-from theano.printing import Print
+from theano import tensor as T
+from theano.ifelse import ifelse
 
 class Interval(object):
     
@@ -9,29 +10,133 @@ class Interval(object):
         self.upper = upper
 
     def __getitem__(self, at):
+        """Returns specified slice of interval as a interval.
+
+        at: Coordinates / slice to be taken.
+        
+        Does not copy data."""
         return Interval(self.lower[at], self.upper[at])
 
-    def __setitem__(self, at, value):
-        self.lower = value.lower[at]
-        self.upper = value.upper[at]
+    def __setitem__(self, at, other):
+        """Just like Theano set_subtensor function, but as a operator.
+        
+        at: Coordinates / slice to be set.
+        other: Data to be put at 'at'
+        
+        """
+        self.lower = T.set_subtensor(self.lower[at], other.lower)
+        self.upper = T.set_subtensor(self.upper[at], other.upper)
 
-    def __len__(self):
-        return len(self.lower)
+    def shape(self):
+        """Returns shape of interval. Checks only 'lower' matrix.
+        
+        Note: does not require self.upper for computations. Therefore it is not
+        safe, but faster."""
+        return self.lower.shape
 
     def __add__(self, other):
+        """Returns sum of two intervals.
+        
+        other: Interval to be added."""
         res_lower = self.lower + other.lower
         res_upper = self.upper + other.upper
         return Interval(res_lower, res_upper)
 
     def __sub__(self, other):
+        """Returns difference between two intervals.
+        
+        other: Interval to be subtracted."""
         res_lower = self.lower - other.upper
         res_upper = self.upper - other.lower
         return Interval(res_lower, res_upper)
 
     def __mul__(self, other):
+        """Returns product of two intervals.
+        
+        other: Interval to be multiplied."""
         ll = self.lower * other.lower
         lu = self.lower * other.upper
         ul = self.upper * other.lower
         uu = self.upper * other.upper
-        return Interval(theano.minumum(ll, lu, ul, uu), 
-                        theano.maximum(ll, lu, ul, uu))
+        l = T.minimum(ll, lu)
+        l = T.minimum(l, ul)
+        l = T.minimum(l, uu)
+        u = T.maximum(ll, lu)
+        u = T.maximum(u, ul)
+        u = T.maximum(u, uu)
+        return Interval(l, u)
+
+    def _has_zero(self):
+        """For any interval in Interval, returns whether is contains zero."""
+        return T.and_(T.lt(self.lower, 0), T.gt(self.upper, 0))
+
+    def reciprocal(self):
+        """Returns reciprocal of the interval.
+
+        It is a partial reciprocal function. Does not allow 0 to be within
+        interval. Should not be treated as general reciprocal function."""
+        # Note: Could be concidered whether not to use input check.
+        # If 0 is within interval, returns 1/0 that, we hope, will throw
+        # some exception on the device. Be careful with this.
+
+        #Input check below couses program interrupt if any _has_zero happened.
+        #return Interval(switch(self._has_zero(),
+        #                       T.constant(1)/T.constant(0),
+        #                       T.inv(self.upper)),
+        #                T.inv(self.lower))
+        return Interval(T.inv(self.upper), T.inv(self.lower))
+
+    def neg(self):
+        return Interval(T.neg(self.upper), T.neg(self.lower))
+
+    def exp(self):
+        """Returns interval representing the exponential of the interval."""
+        return Interval(T.exp(self.lower), T.exp(self.upper))
+
+    def square(self):
+        """For interval I, returns I' such that for any x in I, I' contains
+        x*x and no other.
+        
+        e.g [-1, 1].sq() == [0, 1]"""
+        lsq = self.lower * self.lower
+        usq = self.upper * self.upper
+        u = T.maximum(lsq, usq)
+        l = T.switch(self._has_zero(), 0, T.minimum(lsq, usq))
+        return Interval(l, u)
+
+    def power(exponent):
+        """For interval i, returns i^exponent.
+        
+        exponent: Number to be passed as exponent to i^exponent.
+
+        Note: If interval contains some elements lower/equal to 0, exponent
+        should be integer."""
+        le = T.pow(self.lower, exponent)
+        ue = T.pow(self.upper, exponent)
+        l, u = None, None
+        if isinstance(exponent, int, long):
+            if exponent > 0:
+                if exponent % 2 == 0:
+                    l = T.switch(self._has_zero(), 0, T.minimum(le, ue))
+                    u = T.maximum(le, ue)
+                else:
+                    l = le
+                    u = ue
+            else:
+                if exponent % 2 == 0:
+                    l = T.minimum(le, ue)
+                    u = T.maximum(le, ue)
+                else:
+                    l = ue
+                    u = le
+        else:
+            # Assumes self.lower >= 0. Otherwise it is incorrectly defined.
+            # There is no check.
+            if exponent > 0:
+                l = le
+                u = ue
+            else:
+                l = ue
+                u = le
+        return Interval(l, u)
+
--- athenet/tests/test_interval.py
+++ athenet/tests/test_interval.py
@@ -1,4 +1,5 @@
-
+"""Testing athenet.sparsifying.utils.interval.Interval class with its methods.
+"""
 
 import unittest
 from nose.tools import assert_true, assert_is, assert_equal
@@ -11,11 +12,9 @@ from theano import function
 class IntervalTest(unittest.TestCase):
 
     def test_interval_scalar(self):
-        x = T.dscalar('x')
-        y = T.dscalar('y')
+        x, y = T.dscalars('x', 'y')
         i = Interval(x, y)
-        l = i.lower
-        u = i.upper
+        l, u = i.lower, i.upper
         assert_is(x, l)
         assert_is(y, u)
         z = u - l
@@ -23,11 +22,9 @@ class IntervalTest(unittest.TestCase):
         assert_equal(f(1.1, 3.2), 2.1)
 
     def test_interval_matrix(self):
-        x = T.dmatrix('x')
-        y = T.dmatrix('y')
+        x, y = T.dmatrices('x', 'y')
         i = Interval(x, y)
-        l = i.lower
-        u = i.upper
+        l, u = i.lower, i.upper
         assert_is(x, l)
         assert_is(y, u)
         z = u + l
@@ -37,12 +34,9 @@ class IntervalTest(unittest.TestCase):
         assert_array_equal(res_z, res)
 
     def test_getitem(self):
-        x = T.dmatrix('x')
-        y = T.dmatrix('y')
+        x, y = T.dmatrices('x', 'y')
         i = Interval(x, y)
-        i0 = i[0, 0]
-        i1 = i[1, 1]
-        i2 = i[2, 2]
+        i0, i1, i2 = i[0, 0], i[1, 1], i[2, 2]
         l0, l1, l2 = i0.lower, i1.lower, i2.lower
         u0, u1, u2 = i0.upper, i1.upper, i2.upper
         f = function([x, y], [l0, l1, l2, u0, u1, u2])
@@ -55,6 +49,131 @@ class IntervalTest(unittest.TestCase):
         assert_equal(ru0, 10)
         assert_equal(ru1, 50)
         assert_equal(ru2, 90)
+
+    def test_setitem(self):
+        x, y, z, w = T.dmatrices('x', 'y', 'z', 'w')
+        i1 = Interval(x, y)
+        i2 = Interval(z, w)
+        i1[:, 1:3] = i2
+        f = function([x, y, z, w], [i1.lower, i1.upper])
+        ex_x = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        ex_z = np.array([[20, 30], [50, 60], [80, 90]])
+        ex_y = 100 * ex_x
+        ex_w = 100 * ex_z
+        l, u = f(ex_x, ex_y, ex_z, ex_w)
+        rl = np.array([[1., 20., 30.], [4., 50., 60.], [7., 80., 90.]])
+        ru = rl * 100
+        assert_array_equal(l, rl)
+        assert_array_equal(u, ru)
+
+    def test_shape(self):
+        x, y = T.dmatrices('x', 'y')
+        i1 = Interval(x, y)
+        ex_x = np.array([[2, 3], [5, 6], [8, 9]])
+        ex_y = 10 * ex_x
+        shp = i1.shape()
+        rshp = shp.eval({x: ex_x})
+        assert_equal(len(rshp), 2)
+        assert_equal(rshp[0], 3)
+        assert_equal(rshp[1], 2)
+
+    def test_ops(self):
+        # __add__, __sub__, __mul__
+        x, y, z, w = T.dmatrices('x', 'y', 'z', 'w')
+        i1 = Interval(x, y)
+        i2 = Interval(z, w)
+        l1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        u1 = l1 * 10
+        l2 = l1 * 100
+        u2 = l1 * 1000
+        radd = i1 + i2
+        rsub = i1 - i2
+        rmul = i1 * i2
+        fres = [radd.lower, radd.upper, rsub.lower, rsub.upper, rmul.lower,
+                rmul.upper]
+        f = function([x, y, z, w], fres)
+        addl, addu, subl, subu, mull, mulu = f(l1, u1, l2, u2)
+        raddl = l1 + l2
+        raddu = u1 + u2
+        rsubl = l1 - u2
+        rsubu = u1 - l2
+        rmull = l1 * l2
+        rmulu = u1 * u2
+        assert_array_equal(addl, raddl)
+        assert_array_equal(addu, raddu)
+        assert_array_equal(subl, rsubl)
+        assert_array_equal(subu, rsubu)
+        assert_array_equal(mull, rmull)
+        assert_array_equal(mulu, rmulu)
+
+    def test_op2(self):
+        # reciprocal, neg, exp, sq
+        x, y = T.dmatrices('x', 'y')
+        i = Interval(x, y)
+        reciprocal = i.reciprocal()
+        neg = i.neg()
+        exp = i.exp()
+        sq = i.square()
+        recl, recu = reciprocal.lower, reciprocal.upper
+        negl, negu = neg.lower, neg.upper
+        expl, expu = exp.lower, exp.upper
+        sql, squ = sq.lower, sq.upper
+        l = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=float)
+        u = l + 10.0
+        f = function([x, y], [recl, recu, negl, negu, expl, expu, sql, squ])
+        rrecl, rrecu, rnegl, rnegu, rexpl, rexpu, rsql, rsqu = f(l, u)
+        threcl = np.reciprocal(u)
+        threcu = np.reciprocal(l)
+        thnegl = -u
+        thnegu = -l
+        thexpl = np.exp(l)
+        thexpu = np.exp(u)
+        thsql = np.square(l)
+        thsqu = np.square(u)
+        assert_array_equal(rrecl, threcl)
+        assert_array_equal(rrecu, threcu)
+        assert_array_equal(rnegl, thnegl)
+        assert_array_equal(rnegu, thnegu)
+        assert_array_equal(rexpl, thexpl)
+        assert_array_equal(rexpu, thexpu)
+        assert_array_equal(rsql, thsql)
+        assert_array_equal(rsqu, thsqu)
+
+    def test_op3(self):
+        #power
+        x, y = T.vectors('x', 'y')
+        i = Interval(x, y)
+        v1l = np.array([-3, -2, -1, 0, 1, 2, 3])
+        v1u = v1l + 2
+        v2l = np.array([0, 1, 2])
+        v2u = v2l + 2
+        v3l = np.array([1, 2])
+        v3u = v3l + 2
+        fr1 = i.power(-2.5)
+        fr2 = i.power(-2.)
+        fr3 = i.power(2.)
+        fr4 = i.power(2.5)
+        fr5 = i.power(-2)
+        fr6 = i.power(-3)
+        fr7 = i.power(2)
+        fr8 = i.power(3)
+        f1 = function([x, y], r1)
+        f2 = function([x, y], r2)
+        f3 = function([x, y], r3)
+        f4 = function([x, y], r4)
+        f5 = function([x, y], r5)
+        f6 = function([x, y], r6)
+        f7 = function([x, y], r7)
+        f8 = function([x, y], r8)
+        r1 = f1(v3l, v3u)
+        r2 = f2(v3l, v3u)
+        r3 = f3(v3l, v3u)
+        r4 = f4(v3l, v3u)
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2, catchbreak=True)
