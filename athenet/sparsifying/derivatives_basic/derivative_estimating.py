1d9ee985a0c7d772bde87b718a411dc87f37c296
Wojciech Głogowski <wojciech.glogowski@students.mimuw.edu.pl>
2016-02-29 20:16:27 +0100

    comment fix

--- athenet/sparsifying/derivatives_basic/derivative_estimating.py
+++ athenet/sparsifying/derivatives_basic/derivative_estimating.py
@@ -1,6 +1,5 @@
-"""For any neuron, we calculate its range of activation for inputs of neural
-networks in given range. For simplicity, we assume it is [0, 255]. We do it
-from the beginning to the end of the network."""
+"""For any neuron, we calculate its range of derivative of output with respect
+to this neuron. We do it from the end to the beginning of the network. """
 
 import numpy as np
 import theano
