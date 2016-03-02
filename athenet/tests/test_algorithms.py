fb163bc83db38a59f5dddd09f2ebfa066163e29e
Patryk Czajka <Patryk.Czajka@student.uw.edu.pl>
2016-03-02 03:33:50 +0300

    Add tests for sender and sender2

--- athenet/tests/test_algorithms.py
+++ athenet/tests/test_algorithms.py
@@ -83,20 +83,69 @@ class TestUtils(TestCase):
                     self.assertEqual(layer.W[r][c], 1)
 
 
-class TestSender(TestCase):
-    def test_algorithm(self):
-        print np.zeros((3, 20))
-        print list_of_percentage_rows_table(np.zeros((3, 20)), 5)
-        #net = get_simple_network()
-        #for layer in net.weighted_layers:
-            #layer.W = np.ones(layer.W.shape, dtype=theano.config.floatX)
-            #print layer.W.shape
-        #W = net.weighted_layers[-1].W
-        #W[0][1] = 200
-        #net.weighted_layers[-1].W = W
-        #simple_neuron_deleter(net, (0.5, 0.5))
-        #for layer in net.weighted_layers:
-            #print layer.W
+def get_prepared_network():
+    """return simple network filled with ones and with 200 in 3rd row 1st
+       column of last layer
+    """
+    net = get_simple_network()
+    for layer in net.weighted_layers:
+        layer.W = np.ones(layer.W.shape, dtype=theano.config.floatX)
+    W = net.weighted_layers[-1].W
+    W[3][1] = 200
+    net.weighted_layers[-1].W = W
+    return net
+
+
+def check_params(net, p, layer_limit):
+    all_neurons = 0
+    all_deleted = 0
+    for layer in net.weighted_layers:
+        if not isinstance(layer, FullyConnectedLayer):
+            continue
+        neurons = layer.W.shape[0]
+        deleted = 0
+        for r in xrange(layer.W.shape[0]):
+            for c in xrange(layer.W.shape[1]):
+                if layer.W[r][c] != 0:
+                    break
+            else:
+                deleted += 1
+
+        all_neurons += neurons
+        all_deleted += deleted
+        if deleted / 1. / neurons > layer_limit:
+            return False
+
+    return all_deleted / 1. / all_neurons <= p
+
+
+class TestAlgorithms(TestCase):
+    def test_sender(self):
+        params = [((0.4, 0.5), (0.4, 0.5)),
+                  ((1., 0.5), (0.5, 0.5)),
+                  ((0.1, 0.2), (0.1, 0.2)),
+                  ((0.7, 0.75), (0.7, 0.75)),
+                  ((0.75, 0.6), (0.6, 0.6))]
+
+        for config, (check_p, check_layer_limit) in params:
+            net = get_prepared_network()
+            simple_neuron_deleter(net, config)
+            self.assertTrue(check_params(net, check_p, check_layer_limit))
+            self.assertTrue(net.weighted_layers[-1].W[3][0] != 0)
+
+    def test_sender2(self):
+        params = [((0.4, 0.5), (0.4, 0.5)),
+                  ((1., 0.5), (0.5, 0.5)),
+                  ((0.1, 0.2), (0.1, 0.2)),
+                  ((0.7, 0.75), (0.7, 0.75)),
+                  ((0.75, 0.6), (0.6, 0.6))]
+
+        for config, (check_p, check_layer_limit) in params:
+            net = get_prepared_network()
+            simple_neuron_deleter2(net, config)
+            self.assertTrue(check_params(net, check_p, check_layer_limit))
+            self.assertTrue(net.weighted_layers[-1].W[3][0] != 0)
+
 
 if __name__ == '__main__':
     main(verbosity=2, catchbreak=True)
