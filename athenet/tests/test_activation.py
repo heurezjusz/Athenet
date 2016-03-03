0d01dc7fe8f44a9c5f6a7cd486bb30a9dd643c08
Wojciech Głogowski <wojciech.glogowski@students.mimuw.edu.pl>
2016-03-03 16:22:24 +0100

    auxiliary test functions for fc

--- athenet/tests/test_activation.py
+++ athenet/tests/test_activation.py
@@ -4,8 +4,8 @@
 import unittest
 from nose.tools import assert_true, assert_is, assert_equal
 import numpy as np
-from numpy.testing import assert_array_equal as are,
-                          assert_array_almost_equal as arae
+from numpy.testing import assert_array_equal as are, \
+    assert_array_almost_equal as arae
 from theano import function
 import theano.tensor as T
 from athenet.sparsifying.utils.interval import Interval as I
@@ -14,17 +14,47 @@ from athenet.sparsifying.derest.activation import *
 A = np.array
 
 class FullyConnectedActivationTest(unittest.TestCase):
-    #TODO: Setup?
-    def setUp(self):
-        self.v =  np.arange(24) + 3.0
 
-    def test_1D_simple(self):
-        arae(fully_connected(1, 2, 0), 2)
+    def prepare(self):
+        self.v = np.arange(24) + 3.0
+        self.at_v = 0
+        return self.s, self.v
 
-    def test_2d_used_1D_of_weights(self):
+    def s(self):
+        if self.at_v >= len(self.v):
+            raise TypeError
+        ret = self.v[self.at_v]
+        self.at_v += 1
+        return ret
+
+    def test1DSimple(self):
+        s, v = self.prepare()
+        #arae(fully_connected(1, 2, 0), 2)
+
+    def test2DSimpleUsed1DOfWeights(self):
+        s, v = self.prepare()
+        print s(), s(), s(), s()
+        inp, w, b = A([[s()], [s()]]), A([[s()], s()]), 1.0
+        #arae(fully_connected(inp, w, b), v[0] * v[2] + v[1] * v[3] + 1.0)
+
+    def test2DSimpleUSed2DOfWeights(self):
+        s, v = self.prepare()
+        #v = self.v
+        i = A([v[0], v[1]])
+
+    def test2DSimple(self):
+        s, v = self.prepare()
+        pass
+        #v = self.v
+
+    def test3D(self):
+        s, v = self.prepare()
         v = self.v
-        inp, w, b = 2, A([[v[0]], [v[1]]]), A([[[v[2]]], [v[3]]]), 1.0
-        arae(fully_connected(inp, w, b), v[0] * v[2] + v[1] * v[3] + 1.0)
+
+    def test3DUsingIntervals(self):
+        s, v = self.prepare()
+        v = self.v
+
 
 class ConvolutionalActivationTest(unittest.TestCase):
     pass
@@ -40,3 +70,6 @@ class LRNActivationTest(unittest.TestCase):
 
 class DropoutActivationTest(unittest.TestCase):
     pass
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2, catchbreak=True)
