c644a557a2774c829db949de6e84abc8b45278d1
Wojciech Głogowski <wojciech.glogowski@students.mimuw.edu.pl>
2016-03-09 17:08:25 +0100

    derivatives comments, try-except format in activation

--- athenet/sparsifying/derest/activation.py
+++ athenet/sparsifying/derest/activation.py
@@ -8,26 +8,38 @@ import theano.tensor as T
 
 # TODO: All functions below will be implemented.
 
-def conv():
+def conv(layer_input, weights, stride=(1, 1), padding=(0, 0), n_groups=1, batch_size=1):
     """Returns estimated activation of convolutional layer."""
-    pass
+    try:
+    except:
 
 def dropout(layer_input, p_dropout):
     """Returns estimated activation of dropout layer."""
-    return layer_input * (1.0 - p_dropout)
+    try:
+        layer_input.op.dropout(p_dropout)
+    except:
+        return layer_input * (1.0 - p_dropout)
 
-def fully_connected():
+def fully_connected(layer_input, weights, biases):
     """Returns estimated activation of fully connected layer."""
+    try:
+    except:
     pass
 
-def norm():
+def norm(input_layer, local_range=5, k=1, alpha=0.0002, beta=0.75):
     """Returns estimated activation of LRN layer."""
+    try:
+    except:
     pass
 
-def pool(layer_input, poolsize, stride):
+def pool(layer_input, poolsize, stride=None):
     """Returns estimated activation of max pool layer."""
+    try:
+    except:
     pass
 
-def softmax():
+def softmax(layer_input):
     """Returns estimated activation of softmax layer."""
+    try:
+    except:
     pass
--- athenet/sparsifying/derest/derivative.py
+++ athenet/sparsifying/derest/derivative.py
@@ -8,19 +8,25 @@ import theano.tensor as T
 # TODO: All functions below will be implemented.
 
 def conv():
+    """Returns estimated impact of convolutional layer on output of network."""
     pass
 
 def dropout():
+    """Returns estimated impact of dropout layer on output of network."""
     pass
 
 def fully_connected():
+    """Returns estimated impact of fully connected layer on output of network."""
     pass
 
 def norm():
+    """Returns estimated impact of LRN layer on output of network."""
     pass
 
 def pool():
+    """Returns estimated impact of max pool layer on output of network."""
     pass
 
 def softmax():
+    """Returns estimated impact of softmax layer on output of network."""
     pass
