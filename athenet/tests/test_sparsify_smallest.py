5ad91d7355bcce8c06b2ccad6ebdee5dd9fe15c2
marta <marta.cieslak.1101@gmail.com>
2016-03-10 09:25:54 +0100

    changed percentages into fractions

--- athenet/tests/test_sparsify_smallest.py
+++ athenet/tests/test_sparsify_smallest.py
@@ -7,6 +7,7 @@ from nose.tools import assert_equal, assert_true
 
 
 class SparsifySmallestTest(unittest.TestCase):
+    epsilon = 10 ** (-5)
 
     def set_network(self, number_of_layers=5, size_of_layer=100):
         layers = [LayerMock(
@@ -15,15 +16,15 @@ class SparsifySmallestTest(unittest.TestCase):
             for x in xrange(number_of_layers)]
         self.network = NetworkMock(weighted_layers=layers)
 
-    def get_percentage_of_zeros_in_layer(self, layer):
-        return 100 - 100 * np.count_nonzero(layer.W.flat) / len(layer.W.flat)
+    def get_fraction_of_zeros_in_layer(self, layer):
+        return 1. - np.count_nonzero(layer.W.flat) / float(len(layer.W.flat))
 
-    def get_percentage_of_zeros_in_network(self, network):
+    def get_fraction_of_zeros_in_network(self, network):
         number_of_nonzeros = sum((np.count_nonzero(layer.W.flat)
                                   for layer in network.weighted_layers))
         number_of_weights = sum((len(layer.W.flat)
                                  for layer in network.weighted_layers))
-        return 100 - 100 * number_of_nonzeros / number_of_weights
+        return 1. - (number_of_nonzeros / float(number_of_weights))
 
     def test_no_change_on_network(self):
         self.set_network()
@@ -45,30 +46,30 @@ class SparsifySmallestTest(unittest.TestCase):
 
     def test_zero_for_all_on_network(self):
         self.set_network()
-        sparsify_smallest_on_network(self.network, 100)
-        assert_equal(self.get_percentage_of_zeros_in_network(self.network),
-                     100)
+        sparsify_smallest_on_network(self.network, 1)
+        assert_equal(self.get_fraction_of_zeros_in_network(self.network), 1)
 
     def test_zero_for_all_on_layers(self):
         self.set_network()
-        sparsify_smallest_on_layers(self.network, 100)
+        sparsify_smallest_on_layers(self.network, 1)
         for layer in self.network.weighted_layers:
-            assert_equal(self.get_percentage_of_zeros_in_layer(layer), 100)
+            assert_equal(self.get_fraction_of_zeros_in_layer(layer), 1)
 
-    def test_percentage_of_zeros_on_network(self):
+    def test_fraction_of_zeros_on_network(self):
         self.set_network()
-        for percentage in [10, 30, 50, 60, 90]:
-            sparsify_smallest_on_network(self.network, percentage)
-            assert_true(self.get_percentage_of_zeros_in_network(self.network)
-                        == percentage)
+        for fraction in [0.1, 0.3, 0.5, 0.6, 0.9]:
+            sparsify_smallest_on_network(self.network, fraction)
+            zeros_fraction = self.get_fraction_of_zeros_in_network(
+                self.network)
+            assert_true(abs(zeros_fraction - fraction) <= self.epsilon)
 
-    def test_percentage_of_zeros_on_layers(self):
+    def test_fraction_of_zeros_on_layers(self):
         self.set_network()
-        for percentage in [10, 30, 50, 60, 90]:
-            sparsify_smallest_on_layers(self.network, percentage)
+        for fraction in [0.1, 0.3, 0.5, 0.6, 0.9]:
+            sparsify_smallest_on_layers(self.network, fraction)
             for layer in self.network.weighted_layers:
-                assert_true(self.get_percentage_of_zeros_in_layer(layer)
-                            == percentage)
+                zeros_fraction = self.get_fraction_of_zeros_in_layer(layer)
+                assert_true(abs(zeros_fraction - fraction) <= self.epsilon)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2, catchbreak=True)
