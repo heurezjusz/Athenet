95530be5a6f3877ef1fce0702411379b29654463
marta <marta.cieslak.1101@gmail.com>
2016-04-04 18:07:18 +0200

    better import

--- athenet/algorithm/__init__.py
+++ athenet/algorithm/__init__.py
@@ -1,5 +1,10 @@
 from simple_neuron_deleter import simple_neuron_deleter
 from simple_neuron_deleter2 import simple_neuron_deleter2
-from sparsify_smallest import *
-from utils import *
+from sparsify_smallest import sparsify_nearest_to_layer_mean,\
+    sparsify_nearest_to_network_mean, sparsify_smallest_on_layers,\
+    sparsify_smallest_on_network, get_smallest_indicators,\
+    get_nearest_to_global_mean_indicators,\
+    get_nearest_to_layer_mean_indicators,\
+    get_nearest_to_layers_mean_indicators
+from sharpen_filters import sharpen_filters, get_filters_indicators
 from derest import *
