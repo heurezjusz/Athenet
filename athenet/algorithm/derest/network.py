4c4b649e43e8cc48734737c8c8ad36a21f64cba4
marta <marta.cieslak.1101@gmail.com>
2016-04-16 23:19:31 +0200

    minor fixes

--- athenet/algorithm/derest/network.py
+++ athenet/algorithm/derest/network.py
@@ -1,8 +1,8 @@
-from athenet.network import Network
-from athenet.layers import *
+import theano
+
+from athenet.layers import ConvolutionalLayer, PoolingLayer, FullyConnectedLayer, Softmax, Dropout, ReLU, LRN
 from athenet.algorithm.derest.activation import *
 from athenet.algorithm.derest.derivative import *
-
 
 def _change_order(a):
     """
@@ -15,11 +15,24 @@ def _change_order(a):
         return a
 
 
-class DerestNetwork(Network):
+def _add_tuples(a, b):
+    if not isinstance(a, tuple):
+        a = (a, )
+    if not isinstance(b, tuple):
+        b = (b, )
+    return a + b
+
+
+class DerestNetwork():
 
     def __init__(self, network):
         self.network = network
         self.layers = [DerestLayer(layer) for layer in network.layers]
+
+    def _get_layer_input_shape(self, i):
+        if i > 0:
+            return self.layers[i - 1].layer.output_shape
+        return self.layers[i].layer.input_shape
 
     def count_activations(self, inp):
         for layer in self.layers:
@@ -27,18 +40,14 @@ class DerestNetwork(Network):
         return inp
 
     def count_derivatives(self, outp, batches):
-
         #we assume that batches is equal to outp.shape[0] (for now)
         for i in range(len(self.layers) - 1, -1, -1):
-            if i > 0:
-                input_shape = self.layers[i - 1].layer.output_shape
-            else:
-                input_shape = self.layers[i].layer.input_shape
-            outp = self.layers[i].count_derivatives(outp, batches, input_shape)
+            input_shape = _add_tuples(batches, self._get_layer_input_shape(i))
+            outp = self.layers[i].count_derivatives(outp, input_shape)
         return outp
 
 
-class DerestLayer(Layer):
+class DerestLayer():
 
     def __init__(self, layer):
         self.layer = layer
@@ -50,7 +59,7 @@ class DerestLayer(Layer):
             self.activations = conv(
                 input, _change_order(self.layer.input_shape),
                 self.layer.W, _change_order(self.layer.filter_shape),
-                self.layer.b, self.layer.stride, self.layer.padding
+                theano.shared(self.layer.b), self.layer.stride, self.layer.padding
             )
         elif isinstance(self.layer, Dropout):
             self.activations = dropout(input, self.layer.p_dropout)
@@ -77,15 +86,10 @@ class DerestLayer(Layer):
 
         return self.activations
 
-    def count_derivatives(self, output, batches, input_shape):
-        if ( isinstance(input_shape, tuple)):
-            input_shape2 = (batches, ) + _change_order(input_shape)
-        else:
-            input_shape2 = (batches, input_shape)
-
+    def count_derivatives(self, output, input_shape):
         if isinstance(self.layer, ConvolutionalLayer):
             self.derivatives = d_conv(
-                output, input_shape2,
+                output, input_shape,
                 _change_order(self.layer.filter_shape), self.layer.W,
                 self.layer.stride, self.layer.padding, self.layer.n_groups
             )
@@ -93,16 +97,16 @@ class DerestLayer(Layer):
             self.derivatives = d_dropout(output, self.layer.p_dropout)
         elif isinstance(self.layer, FullyConnectedLayer):
             self.derivatives = d_fully_connected(output, self.layer.W,
-                                                 _change_order(input_shape))
+                                                 input_shape)
         elif isinstance(self.layer, LRN):
             self.derivatives = d_norm(
-                output, self.activations, input_shape2,
+                output, self.activations, input_shape,
                 self.layer.local_range, self.layer.k, self.layer.alpha,
                 self.layer.beta
             )
         elif isinstance(self.layer, PoolingLayer):
             self.derivatives = d_pool(
-                output, self.activations, input_shape2,
+                output, self.activations, input_shape,
                 self.layer.poolsize, self.layer.stride, self.layer.padding,
                 self.layer.mode
             )
--- athenet/algorithm/derest/weights.py
+++ athenet/algorithm/derest/weights.py
@@ -1,14 +1,34 @@
 """Functions counting cost of weights in view of their activation/derivative.
 """
+import numpy
+import theano
 
 from athenet.algorithm.derest.network import DerestNetwork
-from athenet.algorithm.numlike.numlike import Numlike
+from athenet.algorithm.numlike.interval import Interval
+from athenet.network import Network
+from athenet.layers import ConvolutionalLayer, FullyConnectedLayer, Softmax
 
+
+def run_derest_run():
+    n = Network([
+        ConvolutionalLayer(image_shape=(8, 8, 1), filter_shape=(5, 4, 1)),
+#        ReLU(),
+#        MaxPool(poolsize=(2, 2)),
+        FullyConnectedLayer(n_out=10),
+        Softmax()
+    ])
+    n2 = DerestNetwork(n)
+
+    inp = Interval(theano.shared(numpy.full((1, 8, 8), 0.)), theano.shared(numpy.full((1, 8, 8), 1.)))
+    n2.count_activations(inp)
+
+    r = numpy.array([[1., 0., 0., 0., 0., 0., 0., 0., 0., 0.]])
+    out = Interval(theano.shared(r), theano.shared(r))
+    print n2.count_derivatives(out, 1)
 
 def get_derest_indicators(network, input):
-    n = DerestNetwork(network)
-    n.count_activations(input)
-    for outp in input.derest_output(10):
-        n.count_derivatives(outp)
+    pass
+
+
 
 
