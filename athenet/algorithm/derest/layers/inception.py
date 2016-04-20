79085b4a4d014a91710d0bb2e308993a5c51b2b7
Patryk Czajka <Patryk.Czajka@student.uw.edu.pl>
2016-04-20 21:46:06 +0300

    inception: init and count_activation

--- athenet/algorithm/derest/layers/inception.py
+++ athenet/algorithm/derest/layers/inception.py
@@ -1,32 +1,31 @@
+import theano.tensor as T
 from athenet.algorithm.derest.layers.layer import DerestLayer
+from athenet.algorithm.derest.utils import get_derest_layer
 
 
 class DerestInceptionLayer(DerestLayer):
+
+    def __init__(self, layer):
+        super(DerestInceptionLayer, layer)
+        self.derest_layer_lists = []
+        for layer_list in self.layer.layers_lists:
+            derest_layer_list = []
+            for l in layer_list:
+                derest_layer_list.append(get_derest_layer(l))
+            self.derest_layer_lists.append(derest_layer_list)
+
     def count_activation(self, input):
-        assert NotImplementedError
+        results = []
+        for derest_layer_list in self.derest_layer_lists:
+            inp = input
+            for derest_layer in derest_layer_list:
+                derest_layer.activation = inp
+                inp = derest_layer.count_activation(inp)
+            results.append(inp)
+        return T.concatenate(results)
 
     def count_derivatives(self, output, input_shape):
         assert NotImplementedError
 
     def count_derest(self, f):
         raise NotImplementedError
-
-
-def a_inception(layer_input, layer):
-    """Returns estimated activation of inception layer.
-
-    :param Numlike layer_input: input
-    :rtype: Numlike
-    :param InceptionLayer layer: layer of which activation will be retuned
-    """
-    assert_numlike(layer_input)
-    assert(isinstance(layer, InceptionLayer))
-
-    out = []
-    for layer_list in layer.layer_lists:
-        inp = layer_input
-        for l in layer_list:
-            inp = count_activation(inp, l)
-        out.append(inp)
-
-    return T.concatenate(out, axis=1)
