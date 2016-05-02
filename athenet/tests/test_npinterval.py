9e5188e8606a522923d0c976639fe5a46d486bd2
Patryk Czajka <Patryk.Czajka@student.uw.edu.pl>
2016-05-03 01:05:46 +0300

    Add Sub

--- athenet/tests/test_npinterval.py
+++ athenet/tests/test_npinterval.py
@@ -48,7 +48,8 @@ class TestMultiplying(TestCase):
         self.assertTrue((rl == R.lower).all())
         self.assertTrue((ru == R.upper).all())
 
-        R = B * A
+        R = B
+        R *= A
         self.assertTrue((rl == R.lower).all())
         self.assertTrue((ru == R.upper).all())
 
@@ -127,6 +128,50 @@ class TestAdding(TestCase):
             A = NpInterval(np.ones(shape), 2 * np.ones(shape))
             B = NpInterval(np.ones(shape) * 2, np.ones(shape) * 3)
             R = A + B
+            self.assertEqual(R.shape, shape)
+
+
+class TestSub(TestCase):
+    def test_case(self):
+        al = np.asarray([[1, -2, -1], [-4, -5, -1]])
+        au = np.asarray([[2, -1,  1], [42, -4, 7]])
+        A = NpInterval(al, au)
+
+        bl = np.asarray([[1, 1, 4], [-1, -2, -13]])
+        bu = np.asarray([[2, 2, 5], [-1, -1,   1]])
+        B = NpInterval(bl, bu)
+
+        rl = np.asarray([[-1, -4, -6], [-3, -4, -2]])
+        ru = np.asarray([[ 1, -2, -3], [43, -2, 20]])
+
+        R = A - B
+        self.assertTrue((rl == R.lower).all())
+        self.assertTrue((ru == R.upper).all())
+
+    def _check_result(self, A, B, R):
+        al, au = A.lower[0], A.upper[0]
+        bl, bu = B.lower[0], B.upper[0]
+        rl, ru = R.lower[0], R.upper[0]
+        for a, b in product(xrange(al, au), xrange(bl, bu)):
+            self.assertTrue(rl <= a - b <= ru)
+
+    def test_correct(self):
+        for i in xrange(100):
+            l = [randrange(-10, 10) for j in xrange(4)]
+            A = NpInterval(np.asarray([l[0]]), np.asarray([l[1]]))
+            if A.lower[0] > A.upper[0]:
+                A.lower, A.upper = A.upper, A.lower
+            B = NpInterval(np.asarray([l[0]]), np.asarray([l[1]]))
+            if B.lower[0] > B.upper[0]:
+                B.lower, B.upper = B.upper, B.lower
+            self._check_result(A, B, A - B)
+
+    def test_shape(self):
+        for i in xrange(100):
+            shape = _random_shape()
+            A = NpInterval(np.ones(shape), 2 * np.ones(shape))
+            B = NpInterval(np.ones(shape) * 2, np.ones(shape) * 3)
+            R = A - B
             self.assertEqual(R.shape, shape)
 
 
