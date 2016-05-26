38fbf80f266848a0a8f8b17c845a954523b3f146
marta <marta.cieslak.1101@gmail.com>
2016-05-26 11:55:13 +0200

    more complicated derest demo

--- demo/derest_demo.py
+++ demo/derest_demo.py
@@ -1,20 +1,24 @@
 from athenet.algorithm.derest.derest import derest
 from athenet.network import Network
 from athenet.layers import ConvolutionalLayer, FullyConnectedLayer,\
-    Softmax, ReLU, MaxPool
+    Softmax, ReLU, MaxPool, InceptionLayer, LRN
 
+
+def aggregating_example_function(values):
+    a = values.sum()
+    return a.lower + a.upper
 
 n = Network([
-        ConvolutionalLayer(image_shape=(10, 10, 1), filter_shape=(4, 4, 2)),
-        ReLU(),
-        MaxPool(poolsize=(2, 2)),
-        ConvolutionalLayer(filter_shape=(2, 2, 5)),
-        ReLU(),
-        MaxPool(poolsize=(2, 2)),
-        FullyConnectedLayer(n_out=10),
-        ReLU(),
-        FullyConnectedLayer(n_out=3),
-        Softmax(),
+    ConvolutionalLayer(image_shape=(28, 28, 1), filter_shape=(4, 4, 2)),
+    ReLU(),
+#    LRN(),
+    MaxPool(poolsize=(2, 2)),
+#    InceptionLayer(n_filters=[2, 2, 2, 2, 2, 2]),
+    FullyConnectedLayer(n_out=10),
+    ReLU(),
+    FullyConnectedLayer(n_out=3),
+    Softmax(),
 ])
 
-derest(n, 0.6)
+derest(n, 0.6, count_function=aggregating_example_function, max_batch_size=None,
+       normalize_activations=True, normalize_derivatives=True)
