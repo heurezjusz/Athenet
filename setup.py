41f3b7ab433feb22df1628ff283e7d7e5dda5936
Wojciech Głogowski <wojciech.glogowski@students.mimuw.edu.pl>
2016-05-26 17:30:11 +0200

    requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 
 setup(name='Athenet',
       # version = "0.1dev",
-      author="Marta Cieślak, Patryk Czajka, Wojciech Głogowski, Marcin Papierzyński",
+      author="Marta Cieslak, Patryk Czajka, Wojciech Glogowski, Marcin Papierzynski",
+      license="BSD 4-clause",
       packages=find_packages(),
       )
