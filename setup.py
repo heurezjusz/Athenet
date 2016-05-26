6952cba85d8357fcc71da87625d079c0af23325d
Wojciech Głogowski <wojciech.glogowski@students.mimuw.edu.pl>
2016-05-26 17:09:24 +0200

    authords

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from setuptools import setup, find_packages
 
 setup(name='Athenet',
       # version = "0.1dev",
+      author="Marta Cieślak, Patryk Czajka, Wojciech Głogowski, Marcin Papierzyński",
       packages=find_packages(),
       )
